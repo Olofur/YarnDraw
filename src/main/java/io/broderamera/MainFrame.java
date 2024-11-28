a6b71ffab514f4792c7faf10da067225616d158b
Olofur <olofhummelgren@protonmail.com>
2024-11-28 12:39:35 +0100

    Add mainframe that gathers all functionalities in a window

---
+++ src/main/java/io/broderamera/MainFrame.java
@@ -0,0 +1,47 @@
+package io.broderamera;
+
+import javax.swing.*;
+//import java.util.ArrayList;
+
+public class MainFrame {
+    //private ArrayList<Color> colors = new ArrayList<>();
+    //private JTextField colorField = new JTextField(10);
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        JSplitPane menuPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+
+        JSplitPane optionPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+
+        // Create grid panel
+        int x = 100; //Integer.parseInt(JOptionPane.showInputDialog("Enter x dimension"));
+        int y = 100; //Integer.parseInt(JOptionPane.showInputDialog("Enter y dimension"));
+
+        ClickableGridPanel grid = new ClickableGridPanel(x, y);
+        JScrollPane scrollGrid = new JScrollPane(grid);
+        mainPane.setLeftComponent(scrollGrid);
+
+        // Create color wheel panel
+        ColorWheel colorWheel = new ColorWheel();
+        menuPane.setTopComponent(colorWheel);
+
+        // Create color palette panel
+        Palette colorPalette = new Palette();
+        optionPane.setTopComponent(colorPalette);
+
+        ControlPanel controlPanel = new ControlPanel();
+        optionPane.setBottomComponent(controlPanel);
+
+        menuPane.setBottomComponent(optionPane);
+        mainPane.setRightComponent(menuPane);
+
+        // Add the split pane to the frame
+        frame.getContentPane().add(mainPane);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
