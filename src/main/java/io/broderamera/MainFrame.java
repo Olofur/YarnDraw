0f41ec6d75850fc2cde9cb13a7ce3a64c02d60c6
Olofur <olofhummelgren@protonmail.com>
2024-12-09 22:45:44 +0100

    Add menubar options for saving, loading and printing to be fully implemented, as well as restructuring and adding javadoc comment with author tag

--- src/main/java/io/broderamera/MainFrame.java
+++ src/main/java/io/broderamera/MainFrame.java
@@ -4,11 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
 
+/**
+ * The main frame of the application
+ * @version 1.0
+ * @author Olofur
+ */
 public class MainFrame {
+    int width; 
+
     public MainFrame(int x, int y) {
         init(x, y);
     }
@@ -18,53 +25,72 @@ public class MainFrame {
     }
 
     public void init(int x, int y) {
+        width = 350;
+
         JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         JSplitPane menuPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         JSplitPane optionPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 
-        // Create color wheel panel
-        ColorWheel colorWheel = new ColorWheel();
-        menuPane.setTopComponent(colorWheel);
-
         // Create color palette panel
-        Palette colorPalette = new Palette();
-        optionPane.setTopComponent(colorPalette);
-
-        ControlPanel controlPanel = new ControlPanel();
+        Palette palette = new Palette(width);
+        ControlPanel controlPanel = new ControlPanel(width);
+        optionPane.setTopComponent(palette);
         optionPane.setBottomComponent(controlPanel);
 
+        // Create color wheel panel
+        ColorWheel colorWheel = new ColorWheel(width);
+        menuPane.setTopComponent(colorWheel);
         menuPane.setBottomComponent(optionPane);
-        mainPane.setRightComponent(menuPane);
 
-        ClickableGridPanel grid = new ClickableGridPanel(x, y);
-        JScrollPane scrollGrid = new JScrollPane(grid);
-        mainPane.setLeftComponent(scrollGrid);
+        // Create grid panel inside scroll pane
+        ZoomableClickableGridPanelTabs gridTabs = new ZoomableClickableGridPanelTabs();
+        mainPane.setLeftComponent(gridTabs);
+        mainPane.setRightComponent(menuPane);
 
         // Set the split pane proportions
         mainPane.setDividerLocation(.99);
         mainPane.setResizeWeight(.99);
-        colorWheel.setMinimumSize(new Dimension(300, 300));
 
-        // Menu bar options
+        colorWheel.setMinimumSize(new Dimension(width, width + 20));
+        palette.setMinimumSize(new Dimension(width, 500));
+
+        // Add menu bar options
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem saveItem = new JMenuItem("Save");
+        saveItem.addActionListener(new ActionListener () {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                controlPanel.savePatternToFile();
+            }
+        });
         JMenuItem loadItem = new JMenuItem("Load");
+        loadItem.addActionListener(new ActionListener () {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                controlPanel.loadPatternFromFile();
+            }
+        });
         JMenuItem printItem = new JMenuItem("Print");
         JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(new ActionListener () {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
         fileMenu.add(saveItem);
         fileMenu.add(loadItem);
         fileMenu.add(printItem);
         fileMenu.add(exitItem);
         menuBar.add(fileMenu);
-        frame.setJMenuBar(menuBar);
 
-        // Add the split pane to the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(mainPane);
-
+        frame.setJMenuBar(menuBar);
         frame.pack();
         frame.setVisible(true);
     }
