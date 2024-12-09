01930d1eb0eb6eda0d7b336024d8e2083310a8b2
Olofur <olofhummelgren@protonmail.com>
2024-12-09 22:50:24 +0100

    Add getter functions for stack, manager, color symbol map. Add active tab variable and remove redundant setter functions for active variables. Also refactoring and add author tag

--- src/main/java/io/broderamera/Palette.java
+++ src/main/java/io/broderamera/Palette.java
@@ -17,28 +17,31 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
+/**
+ * @author Olofur
+ */
 public class Palette extends JPanel{
-    private static HashMap<Integer, ColorSymbol> biglyMap;
-
-    private static JPanel palettePanel;
-
     private static int activeKey;
     private static Color activeColor;
     private static BufferedImage activeSymbol;
+    private static int activeClickableGridPanel;
 
-    private static Color backgroundColor;
-    private static Color borderColor;
+    private static Color backgroundColor = Color.WHITE;
+    private static Color borderColor = Color.BLACK;
 
-    private SvgStack stack;
-    private ImageManager manager;
+    private static SvgStack stack;
+    private static ImageManager manager;
+    private static HashMap<Integer, ColorSymbol> colorSymbolMap;
 
-    //private ArrayList<ColorSymbol> colorSymbols;
+    private static JPanel palettePanel;
 
     public Palette() {
-        backgroundColor = Color.WHITE;
-        borderColor = Color.BLACK;   
-        setPreferredSize(new Dimension(300, 600));
-        // Subpanel of all added colors
+        this(200);
+    }
+
+    public Palette(int width) {
+        activeClickableGridPanel = 0;
+
         palettePanel = new JPanel();
         palettePanel.setLayout(new GridLayout(0, 3));
         palettePanel.addMouseListener(new MouseAdapter() {
@@ -49,32 +52,29 @@ public class Palette extends JPanel{
                 for (int i = 0; i < palettePanel.getComponentCount(); i++) {
                     ColorPanel panel = (ColorPanel) palettePanel.getComponent(i);
                     if (panel.getBounds().contains(x, y)) {
-                        setActive(panel.getKey(), panel.getColor(), panel.getSymbol());
-                        String hexColor = String.format("#%06X", (0xFFFFFF & panel.getColor().getRGB()));
-                        insertText(hexColor);
+                        setActive(panel.key());
+                        String hexColor = getHexFromColor(panel.color());
+                        insertTextField(hexColor);
                         updateBorderHighlight();
                     }
                 }
             }
         });
 
-        // Initialize add and remove buttons
+        // Initialize add, remove and color buttons
         add(addButton());
         add(removeButton());
-
-        // Initialize palette
         add(palettePanel);
+        setPreferredSize(new Dimension(width, 500));
 
-        // Initialize stack
+        // Initialize stack, imager manager and hashmap
         stack = initializeNewStack();
         manager = new ImageManager();
-
-        // Hashmap of all added colors and symbols
-        biglyMap = new HashMap<Integer, ColorSymbol>();
+        colorSymbolMap = new HashMap<Integer, ColorSymbol>();
 
         // Add background color to palette
-        ColorSymbol basePanel = new ColorSymbol(backgroundColor, null, "None");
-        biglyMap.put(basePanel.getKey(), basePanel);
+        ColorSymbol baseColorSymbol = new ColorSymbol(backgroundColor, null, "None");
+        colorSymbolMap.put(baseColorSymbol.getKey(), baseColorSymbol);
         updatePalette();
     }
 
@@ -90,14 +90,14 @@ public class Palette extends JPanel{
                     if (getKeyForColor(color) == -1) {
                         String item = stack.popItem();
                         BufferedImage symbol = manager.getImage(item);
-                        ColorSymbol cs = new ColorSymbol(color, symbol, item);
-                        addToBiglyMap(cs.getKey(), cs);
+                        ColorSymbol colorSymbol = new ColorSymbol(color, symbol, item);
+                        insertcolorSymbolMap(colorSymbol.getKey(), colorSymbol);
                         updatePalette();
                         }
                 } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(null, "Invalid color code");
                 }
-                System.out.println("Number of colors: " + biglyMap.size());
+                System.out.println("Number of colors: " + colorSymbolMap.size());
             }
         });
         return Button;
@@ -110,7 +110,7 @@ public class Palette extends JPanel{
             @Override
             public void actionPerformed(ActionEvent e) {
                 String colorString = ColorWheel.getColorFieldText();
-                if (!biglyMap.isEmpty()) {
+                if (!colorSymbolMap.isEmpty()) {
                     try {
                         Color color = Color.decode(colorString);
                         int key = getKeyForColor(color);
@@ -119,24 +119,25 @@ public class Palette extends JPanel{
                             return;
                         }
                         // Add symbol back to stack
-                        String symbolName = biglyMap.get(key).symbolName();
+                        String symbolName = colorSymbolMap.get(key).symbolName();
                         stack.addItem(symbolName);
-                        biglyMap.remove(key);
-                        for (Map.Entry<Integer, ColorSymbol> entry : biglyMap.entrySet()) {
+                        colorSymbolMap.remove(key);
+                        for (Map.Entry<Integer, ColorSymbol> entry : colorSymbolMap.entrySet()) {
                             if (entry.getValue().color().equals(color)) {
-                                biglyMap.remove(entry.getKey());
+                                colorSymbolMap.remove(entry.getKey());
                                 break;
                             }
                         }
+                        setActive(key);
                         updatePalette();
-                        // in case the active color is deleted
-                        updateActive();
-                        ClickableGridPanel.updateGrid();
+
+                        ZoomableClickableGridPanel activePanel = ZoomableClickableGridPanelTabs.getPanel(activeClickableGridPanel);
+                        activePanel.updateGrid();
                     } catch (NumberFormatException ex) {
                         JOptionPane.showMessageDialog(null, "Invalid color code");
                     }
                 }
-                System.out.println("Number of colors: " + biglyMap.size());
+                System.out.println("Number of colors: " + colorSymbolMap.size());
             }
         });
         return Button;
@@ -145,17 +146,7 @@ public class Palette extends JPanel{
     public static ColorPanel colorButton(Color color) {
         ColorPanel Button = new ColorPanel();
         int key = getKeyForColor(color);
-        Button.setKey(key);
-        if (ControlPanel.showColor()) {
-            Button.setColor(color);
-        } else {
-            Button.setColor(backgroundColor);
-        }
-        if (ControlPanel.showSymbol()) {
-            Button.setSymbol(biglyMap.get(key).symbol());
-        } else {
-            Button.setSymbol(null);
-        }
+        Button.setStats(key);
         Button.setPreferredSize(new Dimension(100, 50));
         return Button;
     }
@@ -177,8 +168,8 @@ public class Palette extends JPanel{
     public static void updatePalette() {
         // Do not remuve all, instead see what has changed and update only that
         palettePanel.removeAll();
-        for (int key : biglyMap.keySet()) {
-            ColorPanel button = colorButton(biglyMap.get(key).color());
+        for (int key : colorSymbolMap.keySet()) {
+            ColorPanel button = colorButton(colorSymbolMap.get(key).color());
             if (activeKey == key) {
                 button.setBorder(BorderFactory.createLineBorder(Color.BLUE, 4));
             } else {
@@ -190,42 +181,17 @@ public class Palette extends JPanel{
         palettePanel.repaint();
     }
 
-    public static void addToBiglyMap(int key, ColorSymbol cs) {
-        biglyMap.put(key, cs);
-    }
-
     public static void clearPalette() {
-        biglyMap.clear();
+        colorSymbolMap.clear();
         palettePanel.removeAll();
         palettePanel.revalidate();
         palettePanel.repaint();
     }
 
-    public static void updateActive() {
-        int key = getActiveKey();
-        if (!biglyMap.keySet().contains(key)) {
-            key = 1;
-        }
-        Color keyColor = biglyMap.get(key).color();
-        BufferedImage keySymbol = Palette.biglyMap.get(key).symbol();
-        if (ControlPanel.showColor()) {
-            setActiveColor(keyColor);
-        } else {
-            setActiveColor(backgroundColor);
-        }
-        System.out.println("Active color changed to: " + activeColor);
-        if (ControlPanel.showSymbol()) {
-            setActiveSymbol(keySymbol);
-        } else {
-            setActiveSymbol(null);
-        }
-        System.out.println("Active symbol changed to: " + activeSymbol);
-    }
-
     public static void updateBorderHighlight() {
         for (int i = 0; i < palettePanel.getComponentCount(); i++) {
             ColorPanel panel = (ColorPanel) palettePanel.getComponent(i);
-            if (activeKey == panel.getKey()) {
+            if (activeKey == panel.key()) {
                 panel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 4));
             } else {
                 panel.setBorder(BorderFactory.createLineBorder(getBorderColor()));
@@ -233,32 +199,41 @@ public class Palette extends JPanel{
         }
     }
 
-    public static void setActive(int key, Color color, BufferedImage symbol) {
-        activeKey = key;
-        System.out.println("Active key changed to: " + activeKey);        setActiveColor(color);
-        activeColor = color;
+    public static void setActive(int key) {
+        if (colorSymbolMap.keySet().contains(key)) {
+            activeKey = key;
+        } else {
+            activeKey = 1;
+        }
+        System.out.println("Active key changed to: " + activeKey);
+        if (ControlPanel.displayColor()) {
+            activeColor = colorSymbolMap.get(activeKey).color();
+        } else {
+            activeColor = backgroundColor;
+        }
         System.out.println("Active color changed to: " + activeColor);
-        activeSymbol = symbol;
+        if (ControlPanel.displaySymbol()) {
+            activeSymbol = Palette.colorSymbolMap.get(activeKey).symbol();
+        } else {
+            activeSymbol = null;
+        }
         System.out.println("Active symbol changed");
     }
 
+    public static void setActiveClickableGridPanel(int key) {
+        activeClickableGridPanel = key;
+    }
+
+    public static int getActiveClickableGridPanel() {
+        return activeClickableGridPanel;
+    }
 
     public static int getActiveKey() {
         return activeKey;
     }
 
-    public static void setActiveColor(Color color) {
-        activeColor = color;
-        System.out.println("Active color changed to: " + activeColor);
-    }
-
     public static Color getActiveColor() {
         return activeColor;
-    }
-
-    public static void setActiveSymbol(BufferedImage symbol) {
-        activeSymbol = symbol;
-        System.out.println("Active symbol changed");
     }
 
     public static BufferedImage getActiveSymbol() {
@@ -273,8 +248,20 @@ public class Palette extends JPanel{
         return borderColor;
     }
 
+    public static SvgStack getStack() {
+        return stack;
+    }
+
+    public static ImageManager getManager() {
+        return manager;
+    }
+
+    public static HashMap<Integer, ColorSymbol> getcolorSymbolMap() {
+        return colorSymbolMap;
+    }
+
     public static int getKeyForColor(Color color) {
-        for (Map.Entry<Integer, ColorSymbol> entry : biglyMap.entrySet()) {
+        for (Map.Entry<Integer, ColorSymbol> entry : colorSymbolMap.entrySet()) {
             if (entry.getValue().color().equals(color)) {
                 return entry.getKey();
             }
@@ -283,18 +270,23 @@ public class Palette extends JPanel{
         return -1;
     }
 
-    public static HashMap<Integer, ColorSymbol> getBiglyMap() {
-        return biglyMap;
+    public static String getHexFromColor(Color color) {
+        return String.format("#%06X", (0xFFFFFF & color.getRGB()));
     }
 
-    public static void insertText(String text) {
+    public static void insertcolorSymbolMap(int key, ColorSymbol colorSymbol) {
+        colorSymbolMap.put(key, colorSymbol);
+    }
+
+    public static void insertTextField(String text) {
         ColorWheel.setColorFieldText(text);
     }
 
     public static void main(String[] args) {
+        Palette panel = new Palette();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Palette panel = new Palette();
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
