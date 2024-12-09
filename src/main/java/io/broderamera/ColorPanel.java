979c61c4d4c9e3743ad5e2d661fb0366bacf7e30
Olofur <olofhummelgren@protonmail.com>
2024-12-09 22:38:53 +0100

    Refactoring and remove some setter functions

--- src/main/java/io/broderamera/ColorPanel.java
+++ src/main/java/io/broderamera/ColorPanel.java
@@ -5,6 +5,9 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.awt.image.BufferedImage;
 
+/**
+ * @author Olofur
+ */
 public class ColorPanel extends JPanel {
     private int key;
     private Color color;
@@ -28,65 +31,52 @@ public class ColorPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         super.setBackground(color);
-        g.drawImage(symbol, 0, 0, getWidth(), getHeight(), null);      
+        g.drawImage(symbol, 0, 0, getWidth(), getHeight(), null);
     }
 
     public void setStats(int key) {
         this.key = key;
 
-        if (ControlPanel.showColor()) {
-            this.color = Palette.getBiglyMap().get(key).color();
+        if (ControlPanel.displayColor()) {
+            this.color = Palette.getcolorSymbolMap().get(key).color();
         } else {
             this.color = Palette.getBackgroundColor();
         }
-        if (ControlPanel.showSymbol()) {
-            this.symbol = Palette.getBiglyMap().get(key).symbol();
+        if (ControlPanel.displaySymbol()) {
+            this.symbol = Palette.getcolorSymbolMap().get(key).symbol();
         } else {
             this.symbol = null;
         }
     }
 
     public boolean isRepresentedWrongly() {
-        if (ControlPanel.showColor() 
-                && this.color != Palette.getBiglyMap().get(key).color()) {
+        if (ControlPanel.displayColor() 
+                && this.color != Palette.getcolorSymbolMap().get(key).color()) {
             return true;
-        } else if (!ControlPanel.showColor() 
+        } else if (!ControlPanel.displayColor() 
                 && this.color != Palette.getBackgroundColor()) {
             return true;
-        } else if (ControlPanel.showSymbol() 
-                && this.symbol != Palette.getBiglyMap().get(key).symbol()) {
+        } else if (ControlPanel.displaySymbol() 
+                && this.symbol != Palette.getcolorSymbolMap().get(key).symbol()) {
             return true;
-        } else if (!ControlPanel.showSymbol() 
+        } else if (!ControlPanel.displaySymbol() 
                 && this.symbol != null) {
-            if (Palette.getBiglyMap().get(key).symbol() != null) {
+            if (Palette.getcolorSymbolMap().get(key).symbol() != null) {
                 return true;
             }
         }
         return false;
     }
 
-    public int getKey() {
+    public int key() {
         return key;
     }
 
-    public void setKey(int key) {
-        this.key = key;
-
-    }
-
-    public Color getColor() {
+    public Color color() {
         return color;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public BufferedImage getSymbol() {
+    public BufferedImage symbol() {
         return symbol;
-    }
-
-    public void setSymbol(BufferedImage symbol) {
-        this.symbol = symbol;
     }
 }
