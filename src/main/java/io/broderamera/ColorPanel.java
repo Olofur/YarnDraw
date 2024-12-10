fd46ffe8f72719ab0da43d2efd9b99125b13c99a
Olofur <olofhummelgren@protonmail.com>
2024-12-10 15:07:52 +0100

    Add comments

--- src/main/java/io/broderamera/ColorPanel.java
+++ src/main/java/io/broderamera/ColorPanel.java
@@ -6,6 +6,20 @@ import javax.swing.JPanel;
 import java.awt.image.BufferedImage;
 
 /**
+ * Represents a color panel extending from the JPanel class. The panel contains
+ * three main properties: key, color, and symbol. They each have their own
+ * getter methods and are all set simultaneously through the setStats method.
+ * The paint component method is overridden to display the color as background 
+ * color and draws the symbol using drawImage.
+ * 
+ * The isRepresentedWrongly method is used to check if the panel is represented
+ * correctly according to the display booleans displayColor and displaySymbol
+ * in the ControlPanel class.
+ * 
+ * The constructor takes either the three properties as arguments or no
+ * arguments. If no arguments are given, the panel will be initialized with
+ * key = 0, color = null, and symbol = null.
+ * 
  * @author Olofur
  */
 public class ColorPanel extends JPanel {
@@ -13,20 +27,12 @@ public class ColorPanel extends JPanel {
     private Color color;
     private BufferedImage symbol;
 
-    public ColorPanel() {
-        super();
-        this.key = 0;
-        this.color = null;
-        this.symbol = null;
-    }
-
-    public ColorPanel(int key, Color color, BufferedImage symbol) {
-        super();
-        this.key = key;
-        this.color = color;
-        this.symbol = symbol;
-    }
-
+    /**
+     * Overrides the paintComponent method to display the color as background
+     * color and draws the symbol using drawImage.
+     * 
+     * @param g
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -34,6 +40,37 @@ public class ColorPanel extends JPanel {
         g.drawImage(symbol, 0, 0, getWidth(), getHeight(), null);
     }
 
+    /**
+     * Default constructor.
+     */
+    public ColorPanel() {
+        super();
+        this.key = 0;
+        this.color = null;
+        this.symbol = null;
+    }
+
+    /**
+     * Sets the three main properties of the panel: key, color, and symbol.
+     * 
+     * @param key
+     * @param color
+     * @param symbol
+     */
+    public ColorPanel(int key, Color color, BufferedImage symbol) {
+        super();
+        this.key = key;
+        this.color = color;
+        this.symbol = symbol;
+    }
+
+    /**
+     * Sets the three main properties of the panel: key, color, and symbol.
+     * Only the key is given as argument, and the other two are found in the
+     * Palette colorSymbolMap, which maps all keys to colors and symbols.
+     * 
+     * @param key
+     */
     public void setStats(int key) {
         this.key = key;
 
@@ -49,6 +86,12 @@ public class ColorPanel extends JPanel {
         }
     }
 
+    /**
+     * Checks if the panel is represented correctly according to the ControlPanel
+     * display booleans displayColor and displaySymbol.
+     * 
+     * @return boolean
+     */
     public boolean isRepresentedWrongly() {
         if (ControlPanel.displayColor() 
                 && this.color != Palette.getcolorSymbolMap().get(key).color()) {
@@ -68,14 +111,29 @@ public class ColorPanel extends JPanel {
         return false;
     }
 
+    /**
+     * Returns the key of the panel.
+     * 
+     * @return int
+     */
     public int key() {
         return key;
     }
 
+    /**
+     * Returns the color of the panel.
+     * 
+     * @return Color
+     */
     public Color color() {
         return color;
     }
 
+    /**
+     * Returns the symbol of the panel.
+     * 
+     * @return BufferedImage
+     */
     public BufferedImage symbol() {
         return symbol;
     }
