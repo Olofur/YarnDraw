5b7cb084a6c25547ef449b40b9fc66e4d0fd480c
Olofur <olofhummelgren@protonmail.com>
2024-12-11 13:09:14 +0100

    Add documentation

--- src/main/java/io/broderamera/ImageManager.java
+++ src/main/java/io/broderamera/ImageManager.java
@@ -9,24 +9,37 @@ import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 
 /**
- * Inspired by
+ * Class for handling SVG images and converting them to BufferedImages. 
+ * 
+ * Inspired by :
  * @link https://web.archive.org/web/20131215231214/http://bbgen.net/blog/2011/06/java-svg-to-bufferedimage/
  * @author Olofur
  */
 public class ImageManager extends ImageTranscoder {
     private BufferedImage image = null;
 
+    /**
+     * Creates a BufferedImage with the given width and height
+     */
     @Override
     public BufferedImage createImage(int width, int height) {
         BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         return buffer;
     }
 
+    /**
+     * Writes an image to the BufferedImage
+     */
     @Override
     public void writeImage(BufferedImage image, TranscoderOutput to) throws TranscoderException {
         this.image = image;
     }
 
+    /**
+     * Returns an image from the resources folder
+     * @param name
+     * @return BufferedImage
+     */
     public BufferedImage getImage(String name) {
         InputStream file = getClass().getResourceAsStream("/" + name);
         TranscoderInput transcoderInput = new TranscoderInput(file);
